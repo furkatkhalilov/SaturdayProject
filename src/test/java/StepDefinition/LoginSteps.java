27a050d01b9a69c9d17922513be907f0d59f65ab
mhmmtkkl <makoklu32@gmail.com>
2020-07-27 19:35:38 -0400

    Day1

---
+++ src/test/java/StepDefinition/LoginSteps.java
@@ -0,0 +1,25 @@
+package StepDefinition;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class LoginSteps {
+
+    @Given("^Navigate to website$")
+    public void navigate_to_website()  {
+
+
+    }
+
+    @When("^Enter the username and password$")
+    public void enter_the_username_and_password() {
+
+    }
+
+    @Then("^User should login successfully$")
+    public void user_should_login_successfully(){
+
+    }
+    
+}
