4e2c92d3d143d3dcfbf5cb7d60ac5cbb947c40c5
mhmmtkkl <makoklu32@gmail.com>
2020-07-27 20:39:44 -0400

    Day1

--- src/test/java/StepDefinition/_03_LoginBasqarStep.java
+++ src/test/java/StepDefinition/_03_LoginBasqarStep.java
@@ -3,11 +3,25 @@ package StepDefinition;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class _03_LoginBasqarStep {
 
+    WebDriver driver;
+
     @Given("^Navigate to basqar$")
     public void navigate_to_basqar() {
+
+        /*
+            Set property - open browser and navigate to website
+         */
+
+        WebDriverManager.chromedriver().setup(); // this line is for System.setproperty
+        driver = new ChromeDriver();
+
+        driver.get("https://test.basqar.techno.study/");
 
     }
 
