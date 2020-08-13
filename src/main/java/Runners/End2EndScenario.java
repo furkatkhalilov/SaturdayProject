c4e3aa9bfc31beb8e81f435aa7368ae3c30de651
mhmmtkkl <makoklu32@gmail.com>
2020-08-13 19:38:01 -0400

    Day9

--- src/main/java/Runners/End2EndScenario.java
+++ src/main/java/Runners/End2EndScenario.java
@@ -10,8 +10,12 @@ package Runners;
 
          */
 
+import com.cucumber.listener.Reporter;
 import cucumber.api.CucumberOptions;
 import cucumber.api.testng.AbstractTestNGCucumberTests;
+import org.testng.annotations.AfterClass;
+
+import java.io.File;
 
 @CucumberOptions(
 
@@ -26,7 +30,19 @@ import cucumber.api.testng.AbstractTestNGCucumberTests;
 
 )
 
-
 public class End2EndScenario extends AbstractTestNGCucumberTests {
+
+        @AfterClass
+        public static void afterClass(){
+
+//                Location of the xml file
+
+                Reporter.loadXMLConfig(new File("src/test/java/FeatureFiles/extentReport.xml"));
+                Reporter.setSystemInfo("User Name" , "Ali Koklu");
+                Reporter.setSystemInfo("Application Name", "Basqar");
+                Reporter.setSystemInfo("Operating System Type" , System.getProperty("os.name"));
+                Reporter.setSystemInfo("Environment", "QA");
+                Reporter.setTestRunnerOutput("Test execution Cucumber report");
+        }
 
 }
