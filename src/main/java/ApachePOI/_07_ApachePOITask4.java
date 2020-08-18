611e95cfadd3631c8457f89ca13d3b299764e72a
mhmmtkkl <makoklu32@gmail.com>
2020-08-17 21:49:26 -0400

    Day11

---
+++ src/main/java/ApachePOI/_07_ApachePOITask4.java
@@ -0,0 +1,22 @@
+package ApachePOI;
+
+public class _07_ApachePOITask4 {
+
+
+    /*
+
+            Get the data in column 1 for each row.
+
+            ali123
+            MyPasswordIsHere
+            123 main
+            12345
+            Harrison
+            New jersey
+
+     */
+
+
+
+
+}
